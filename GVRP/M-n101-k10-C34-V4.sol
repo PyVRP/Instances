f884d49824b26d15cae9fede688186a8015dabcf
Niels Wouda <N-Wouda@users.noreply.github.com>
2024-03-15 20:05:40 +0100

    GVRP instances and BKS (#10)

---
+++ GVRP/M-n101-k10-C34-V4.sol
@@ -0,0 +1,6 @@
+Route #1: 34 37 31 50 51 58 54 57 42
+Route #2: 90 86 84 83 76 73 77 80 74 66 67
+Route #3: 75 1 99 94 96 98
+Route #4: 21 28 18 17 15 14 12 10
+Cost: 458
+Comment: Known optimal, see Freitas et al. (2023; https://doi.org/10.1016/j.cor.2022.106040) for details
---
+++ GVRP/M-n101-k10-C51-V5.sol
@@ -0,0 +1,7 @@
+Route #1: 47 46 50 52 31 35 37 39 36 32 29 20
+Route #2: 65 62 66 68 55 54 56 58 40
+Route #3: 72 80 79 73 71 76 81 83 84 86 89 90
+Route #4: 5 6 2 99 100 97 94 95 98
+Route #5: 22 28 13 17 19 15 14 12 10
+Cost: 542
+Comment: Known optimal, see Freitas et al. (2023; https://doi.org/10.1016/j.cor.2022.106040) for details
