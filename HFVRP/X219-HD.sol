965b48e0856a300bdda384b43b5fc88a1fae305f
Leon Lan <l.lan@vu.nl>
2025-05-15 22:24:44 +0200

    Update BKS

--- HFVRP/X219-HD.sol
+++ HFVRP/X219-HD.sol
@@ -1,86 +1,86 @@
-Route #1: 83 116
-Route #2: 32 39
-Route #3: 190 71
-Route #4: 125 139
-Route #5: 78 18
-Route #6: 76 88
-Route #7: 13 99
-Route #8: 205 36
-Route #9: 56 218
-Route #10: 137 74
-Route #11: 12 70
-Route #12: 7 48
-Route #13: 20 41
-Route #14: 34 130
-Route #15: 198 59
-Route #16: 129 158
-Route #17: 16 167
-Route #18: 173 146
-Route #19: 180 191
-Route #20: 30 166
-Route #21: 3 112
-Route #22: 93 185
-Route #23: 5 176
-Route #24: 9 61
-Route #25: 
-Route #26: 
-Route #27: 
-Route #28: 
-Route #29: 
-Route #30: 
-Route #31: 
-Route #32: 
-Route #33: 
-Route #34: 
-Route #35: 153 165 172
-Route #36: 22 57 209
-Route #37: 37 175 159
-Route #38: 89 168 92
-Route #39: 212 69 136
-Route #40: 72 65 2
-Route #41: 11 14 80
-Route #42: 195 199 182
-Route #43: 103 143 216
-Route #44: 55 54 151
-Route #45: 10 45 111
-Route #46: 117 49 24
-Route #47: 196 120 194
-Route #48: 106 35 67
-Route #49: 81 204 113
-Route #50: 97 1 208
-Route #51: 75 46 114
-Route #52: 157 124 193
-Route #53: 189 179 108
-Route #54: 62 132 188
-Route #55: 95 52 4
-Route #56: 147 162 85
-Route #57: 131 138 183
-Route #58: 178 181 155
-Route #59: 210 201 207
-Route #60: 211 91 134
-Route #61: 156 58 171
-Route #62: 82 98 141
-Route #63: 149 77 177
-Route #64: 26 122 27
-Route #65: 47 123 170
-Route #66: 127 64 51
-Route #67: 192 107 110
-Route #68: 164 115 87
-Route #69: 53 217 102 25
-Route #70: 31 152 96 128
-Route #71: 150 40 38 15
-Route #72: 148 200 33 144
-Route #73: 161 169 43 133
-Route #74: 118 23 29 154
-Route #75: 17 73 174 104
-Route #76: 187 19 213 184
-Route #77: 63 42 44 203
-Route #78: 28 100 145 186
-Route #79: 126 94 68 105
+Route #1: 56 218
+Route #2: 157 87
+Route #3: 20 41
+Route #4: 176 5
+Route #5: 109 107
+Route #6: 7 48
+Route #7: 139 125
+Route #8: 39 32
+Route #9: 129 158
+Route #10: 13 99
+Route #11: 137 74
+Route #12: 71 190
+Route #13: 3 112
+Route #14: 193 124
+Route #15: 18 78
+Route #16: 70 12
+Route #17: 138 131
+Route #18: 93 183
+Route #19: 61 9
+Route #20: 52 95
+Route #21: 30 146
+Route #22: 4 191
+Route #23: 166 185
+Route #24: 173 180
+Route #25:
+Route #26:
+Route #27:
+Route #28:
+Route #29:
+Route #30:
+Route #31:
+Route #32:
+Route #33:
+Route #34:
+Route #35: 67 35 106
+Route #36: 55 54 151
+Route #37: 59 198 130
+Route #38: 86 101 163
+Route #39: 194 120 196
+Route #40: 111 45 10
+Route #41: 136 69 212
+Route #42: 140 60 197
+Route #43: 159 175 37
+Route #44: 177 77 149
+Route #45: 11 14 80
+Route #46: 113 204 81
+Route #47: 72 65 2
+Route #48: 209 57 22
+Route #49: 82 98 141
+Route #50: 24 49 117
+Route #51: 153 165 172
+Route #52: 192 110 167
+Route #53: 83 116 88
+Route #54: 155 181 178
+Route #55: 210 201 207
+Route #56: 115 127 164
+Route #57: 16 64 51
+Route #58: 211 91 134
+Route #59: 97 1 208
+Route #60: 26 122 27
+Route #61: 114 46 75
+Route #62: 85 162 147
+Route #63: 76 36 205
+Route #64: 170 123 47
+Route #65: 62 132 188
+Route #66: 66 160 84
+Route #67: 189 179 108
+Route #68: 89 168 92
+Route #69: 214 202 6 206
+Route #70: 90 135 79 121
+Route #71: 53 217 102 25
+Route #72: 133 43 169 161
+Route #73: 150 40 38 15
+Route #74: 128 96 152 31
+Route #75: 171 58 156 34
+Route #76: 17 73 174 104
+Route #77: 126 94 68 105
+Route #78: 143 186 145 100
+Route #79: 148 200 33 144
 Route #80: 119 215 8 21
-Route #81: 50 142 163 101
-Route #82: 86 197 60 140
-Route #83: 206 6 202 214
-Route #84: 90 135 79 121
-Route #85: 109 84 160 66
-Cost: 120870.09
+Route #81: 63 42 44 203
+Route #82: 118 23 29 154
+Route #83: 216 103 50 142
+Route #84: 187 19 213 184
+Route #85: 199 182 28 195
+Cost: 12075095525
