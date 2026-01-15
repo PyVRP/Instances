1445e12e99fc8c252994e45c902ed61db645fa58
Leon Lan <l.lan@vu.nl>
2026-01-15 16:47:21 +0100

    New BKS v0.13.0 (#13)

--- HFVRP/X148-HVRP.sol
+++ HFVRP/X148-HVRP.sol
@@ -1,56 +1,56 @@
-Route #1: 5
-Route #2: 83
-Route #3: 145 71
-Route #4: 22 55
-Route #5: 118
-Route #6: 99
-Route #7: 39 43
-Route #8: 47
-Route #9: 61 14
-Route #10: 67
-Route #11: 98 7 57
-Route #12: 53 124
-Route #13: 19 31 114
-Route #14: 15 117
-Route #15: 102 131 16
-Route #16: 30 45
-Route #17: 11 63 24 9
-Route #18: 25 29
-Route #19: 33 13 66 36
-Route #20: 49 34
-Route #21: 82 32
-Route #22: 136 104
-Route #23: 137 110
-Route #24: 44
-Route #25: 23 46
-Route #26: 54 10 68
-Route #27: 119 48 6
-Route #28: 12 3
-Route #29: 144 65
-Route #30: 76 28 97
-Route #31: 140 121
-Route #32: 95 87
-Route #33: 59 1
-Route #34: 86 27 111
-Route #35: 84 122
-Route #36: 129 106
-Route #37: 130 26
-Route #38: 100 115 135
-Route #39: 69 38 50 51
-Route #40: 56 132 143
-Route #41: 92 79 139 91 96
-Route #42: 123 146 128
-Route #43: 35 70 113 142
-Route #44: 2 112 17 109
-Route #45: 89 73 126
-Route #46: 8 37 105 40 52
-Route #47: 4 138 94 64 60
-Route #48: 147 141 133 78 41
+Route #1: 55 22
+Route #2: 99
+Route #3: 5
+Route #4: 47
+Route #5: 60 4
+Route #6: 83
+Route #7: 69 63 24
+Route #8: 98 7 57
+Route #9: 118
+Route #10: 124 53
+Route #11: 71 145
+Route #12: 64 94
+Route #13: 114 125
+Route #14: 138 102
+Route #15: 39 43
+Route #16: 136 104
+Route #17: 54 10 68
+Route #18: 45 51
+Route #19: 23 46
+Route #20: 82 32
+Route #21: 25 29
+Route #22: 36 66 13 33
+Route #23: 110 137
+Route #24: 6 48 119
+Route #25: 30 19
+Route #26: 49 34
+Route #27: 44
+Route #28: 3 12
+Route #29: 111 27 86
+Route #30: 67 61 14
+Route #31: 100 115 135
+Route #32: 59 1
+Route #33: 50 9 11 38
+Route #34: 122 84
+Route #35: 106 129
+Route #36: 76 28 97
+Route #37: 26 130
+Route #38: 144 65
+Route #39: 8 37 105 40 52
+Route #40: 35 70 113 142
+Route #41: 87 95 121
+Route #42: 131 16 117 74 15
+Route #43: 92 79 139 91 96
+Route #44: 126 73 89
+Route #45: 109 17 112 2
+Route #46: 31 108 90 116
+Route #47: 128 146 123
+Route #48: 42 75 18 88
 Route #49: 58 72 93 21
-Route #50: 62 85 127 77 134 107
-Route #51: 42 75 18 88
-Route #52: 20 81 120 80 103 101
-Route #53: 108 74 90 116 125
-Route #54: 
-Route #55: 
-Cost: 80285.74
+Route #50: 107 134 77 127 85 62
+Route #51: 20 81 120 80 103 101
+Route #52: 56 132 143 140
+Route #53: 41 78 133 141 147
+Route #54:
+Route #55:
+Cost: 8028527330
